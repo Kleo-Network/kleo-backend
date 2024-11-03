fef70e65827b2c243bb7dc6787598a3986b65b39
princedalsaniya <prince.d@ahduni.edu.in>
2024-11-03 22:43:31 +0530

    fix: CORS errors solved

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 # app/main.py
 from app.mongodb import close_db_connection
 from fastapi import FastAPI
-from app.api.health import router as health_router
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.user_v1 import router as user_router
 from app.settings import settings
 from app.logging_config import setup_logging, logger
@@ -11,8 +11,22 @@ setup_logging()
 
 app = FastAPI(title=settings.PROJECT_NAME)
 
-# Include the API router
-app.include_router(health_router, prefix="/api")
+
+# Add CORS middleware
+origins = [
+    "http://localhost:5173",  # Your local development
+    "https://www.app.kleo.network",  # Your deployed app
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Allows only these origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allows all headers
+)
+
+# Include the API routers
 app.include_router(user_router, prefix="/api/v1/user")
 
 
